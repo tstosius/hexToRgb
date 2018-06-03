48f41c8b6c943a00d0787a22d3704d489d1d768d
Tobias Stosius <stosius.tobias@gmail.com>
2018-06-03 13:20:24 +0200

    [TASK] Add functionality to remove certain input fields

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function convertHex(hex){
-    hex = hex.replace('#','');
-    r = parseInt(hex.substring(0,2), 16);
-    g = parseInt(hex.substring(2,4), 16);
-    b = parseInt(hex.substring(4,6), 16);
+function convertHex(hex) {
+    hex = hex.replace("#", "");
+    r = parseInt(hex.substring(0, 2), 16);
+    g = parseInt(hex.substring(2, 4), 16);
+    b = parseInt(hex.substring(4, 6), 16);
 
-    result = 'rgb('+r+','+g+','+b+')';
+    result = "rgb(" + r + "," + g + "," + b + ")";
     return result;
 }
 
@@ -12,30 +12,30 @@ function invertColor(hexTripletColor) {
     var color = hexTripletColor;
     color = color.substring(1); // remove #
     color = parseInt(color, 16); // convert to integer
-    color = 0x00FF00^ color; // invert three bytes
+    color = 0x00ff00 ^ color; // invert three bytes
     color = color.toString(16); // convert to hex
     color = ("000000" + color).slice(-6); // pad with leading zeros
     color = "#" + color; // prepend #
     return color;
 }
 
-function convert(){
+function convert() {
     var colorArray = [];
     var colorString = "";
     var inputList = document.getElementsByClassName("hexInput");
-    for(var i=0; i<inputList.length; i++) {
-        if(inputList[i].value !== "") {
+    for (var i = 0; i < inputList.length; i++) {
+        if (inputList[i].value !== "") {
             // colorArray.push(convertHex("#"+inputList[i].value));
-            colorString += convertHex("#"+inputList[i].value) + ";";
+            colorString += convertHex("#" + inputList[i].value) + ";";
         }
     }
-    document.getElementById('result').innerHTML = colorString;
+    document.getElementById("result").innerHTML = colorString;
     console.log(colorString);
 }
 
-function createNewInput(color){
+function createNewInput(color) {
     console.log("Create new input");
-    var inputList = document.getElementById("inputList")
+    var inputList = document.getElementById("inputList");
     var newListElement = document.createElement("li");
     newListElement.classList = "inputListItem";
 
@@ -43,21 +43,30 @@ function createNewInput(color){
     newInputElement.setAttribute("type", "text");
     newInputElement.setAttribute("onChange", "updateBackgroundColor(event)");
     newInputElement.classList = "hexInput";
-    if(color) {
+    if (color) {
         newInputElement.value = color;
-        newInputElement.style.background = '#'+color;
+        newInputElement.style.background = "#" + color;
     }
 
+    var deleteInputButton = document.createElement("button");
+    deleteInputButton.innerText = "X";
+    deleteInputButton.setAttribute("onClick", "removeInputElement(event)");
+
     newListElement.appendChild(newInputElement);
+    newListElement.appendChild(deleteInputButton);
     inputList.appendChild(newListElement);
 }
 
-function updateBackgroundColor(event){
-    var newBackgroundColor = '#' + event.srcElement.value;
-    event.srcElement.style.background = newBackgroundColor;
+function removeInputElement(mouseEventInfo) {
+    var inputList = document.getElementById("inputList");
+    inputList.removeChild(mouseEventInfo.srcElement.parentElement);
+    
 }
 
-
+function updateBackgroundColor(event) {
+    var newBackgroundColor = "#" + event.srcElement.value;
+    event.srcElement.style.background = newBackgroundColor;
+}
 
 function initColors() {
     var defaultColorList = [
@@ -66,32 +75,33 @@ function initColors() {
         "#92DCE5",
         "#EEE5E9",
         "#7C7C7C"
-    ]
+    ];
 
     var inputElements = document.getElementsByClassName("hexInput");
-    for(var i=0; i < inputElements.length; i++) {
+    for (var i = 0; i < inputElements.length; i++) {
         inputElements[i].style.background = defaultColorList[i];
     }
 }
 
-function saveColors(){
+function saveColors() {
     var currentColors = [];
     var currentInputs = document.getElementsByClassName("hexInput");
-    for(var i = 0; i < currentInputs.length; i++) {
+    for (var i = 0; i < currentInputs.length; i++) {
         currentColors.push(currentInputs[i].value);
     }
-    window.localStorage.setItem("colors",currentColors);
+    window.localStorage.setItem("colors", currentColors);
 }
 
-function restoreColors(){
+function restoreColors() {
     var storedColors = window.localStorage.getItem("colors");
-    var storedColors = storedColors.split(',');
-    console.log(storedColors);
-    if (storedColors.length > 0){
-        for( var i = 0; i < storedColors.length; i++){
-            createNewInput(storedColors[i]);
+    if (storedColors !== "" && storedColors !== null) {
+        var storedColors = storedColors.split(",");
+        console.log(storedColors);
+        if (storedColors.length > 0) {
+            for (var i = 0; i < storedColors.length; i++) {
+                createNewInput(storedColors[i]);
+            }
         }
-        
     }
 }
 
